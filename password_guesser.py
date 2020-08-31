75a11860909004ee3125938c88795d2cedad291b
Sergio Romeu <sergioromagr@gmail.com>
2020-08-31 17:03:51 +0200

    Checking if your password is secure is done

--- password_guesser.py
+++ password_guesser.py
@@ -1,3 +1,27 @@
+def checkPassword(password):
+    # Flags for checking if a password is strong:
+    # Minimum 8 characters length
+    # Contains 3/4 of the following items: 
+    #   - Uppercase letters, Lowercase letters, numbers or symbols.
+    counter = 0
+    items = []
+    secure = False
+    
+    if len(password) < 8:
+        secure = True
+    else:
+        for x in password:
+            if x.isupper():
+                items.append("Upper")
+            if x.islower():
+                items.append("Lower")
+            if x.isnumeric():
+                items.append("Numeric")
+    
+    items = list(dict.fromkeys(items))
+    print(len(items))
+
+
 def menu():
     out = False
     while out != True:
@@ -7,12 +31,11 @@ def menu():
         print("3. Stop.\n")
         option = int(input("Please choose an option: "))
         if option == 1:
-            
+            password = input("Please enter your password: ")
+            checkPassword(password)
             break
         if option == 3:
             out = True
-
-password = input("Please enter your password: ")
 
 menu()
 
